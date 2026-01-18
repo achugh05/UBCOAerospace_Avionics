8d5d01bcd84719c8f36ab8b3b64e04b0c44aae20
Kieran C <clinekieran@gmail.com>
2026-01-18 14:06:48 -0800

    Added default files, began working on serial

---
+++ football/pi/c++ Classes/Serial.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cline on 2026-01-18.
+//
+
+#include "Serial.h"
---
+++ football/pi/c++ Classes/Serial.h
@@ -0,0 +1,26 @@
+//
+// Created by cline on 2026-01-18.
+//
+
+#ifndef UBCO_FOOTBALL_PI_SERIAL_H
+#define UBCO_FOOTBALL_PI_SERIAL_H
+
+
+struct Packet
+{
+
+};
+
+class Serial
+{
+
+public:
+    void sendPacket();
+
+
+private:
+
+};
+
+
+#endif //UBCO_FOOTBALL_PI_SERIAL_H
